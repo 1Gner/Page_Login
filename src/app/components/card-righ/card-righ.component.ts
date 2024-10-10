a2c8005c068ecd5c33e6024a5da847590b0724df
joao <joao.orosario01@gmail.com>
2024-10-10 00:26:55 -0300

    conectado ao backend

--- src/app/components/card-righ/card-righ.component.ts
+++ src/app/components/card-righ/card-righ.component.ts
@@ -1,18 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { provideHttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-card-righ',
   standalone: true,
   imports: [ReactiveFormsModule, CommonModule],
+  
   templateUrl: './card-righ.component.html',
   styleUrl: './card-righ.component.scss'
 })
 export class CardRighComponent {
   meuForm: FormGroup;
+  message: string = '';
 
-  constructor() {
+  constructor(private authService: AuthService) {
+
     this.meuForm = new FormGroup({
       username: new FormControl(''),
       password: new FormControl(''),
@@ -20,9 +26,32 @@ export class CardRighComponent {
   }
 
 
-  onSubmit() {
-    console.log(this.meuForm.value);
-  }
 
+  onLogin() {
+
+    const username = this.meuForm.get('username')?.value;
+    const password = this.meuForm.get('password')?.value;
+  
+    if (this.meuForm.valid) {
+      this.authService.login(username, password)
+        .pipe(
+          tap(response => {
+           
+            this.message = 'Login bem-sucedido!';
+            console.log(response);
+          })
+        )
+        .subscribe({
+          next: () => {},
+          error: (error) => {
+            this.message = 'Erro ao fazer login. Verifique suas credenciais.';
+            console.error(error);
+          }
+        });
+    } else {
+      this.message = 'Por favor, preencha o username e a senha corretamente.';
+    }
+  }
+  
 
 }
